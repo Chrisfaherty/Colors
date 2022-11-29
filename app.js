e60b7f843df3a2812112400e12fe8ee76bfcfcce
Chrisfaherty <chrismactom@gmail.com>
2022-11-29 20:27:27 +0000

    Add the initial vairables to the js page

---
+++ app.js
@@ -0,0 +1,8 @@
+//Global selection and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = doccument.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+
+
+//Functions
