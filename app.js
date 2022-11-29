0576a2ecb6909347fd0e2539190236cd29f7be34
Chrisfaherty <chrismactom@gmail.com>
2022-11-29 20:38:23 +0000

    Add the chroma color generator to the js file

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
 const sliders = doccument.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
-
+let initialColors;
 
 //Functions
 
 
 //Color Generator
 function generateHex(){
-    const letters = "#0123456789ABCDEF";
-    let hash ="#";
-    for (let i = 0; 1 < 6; i++) {
-        hash += letters[Math.floor(Math.random() * 16)];
-    }
-    return hash;
+    const hexColor = chroma.random();
+    return hexColor
+}
+
+function randomColors(){
+    colorDivs.forEach((div,index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHes();
+    
+        //Add the color to the bg
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
 }
